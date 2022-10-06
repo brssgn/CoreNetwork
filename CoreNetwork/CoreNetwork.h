ea35492afc9e1190fea3bfeb9ce031b1e95e9df0
Boris <brssgn@icloud.com>
2022-10-06 18:05:29 +0300

    Initial commit

---
+++ CoreNetwork/CoreNetwork.h
@@ -0,0 +1,18 @@
+//
+//  CoreNetwork.h
+//  CoreNetwork
+//
+//  Created by Boris Sagan on 06.10.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreNetwork.
+FOUNDATION_EXPORT double CoreNetworkVersionNumber;
+
+//! Project version string for CoreNetwork.
+FOUNDATION_EXPORT const unsigned char CoreNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreNetwork/PublicHeader.h>
+
+
---
+++ CoreNetworkExample/CoreNetworkExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreNetworkExampleApp.swift
+//  CoreNetworkExample
+//
+//  Created by Boris Sagan on 06.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreNetworkExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
